48785036cbd3be1d144142a8aaa7ab130d40331d
jameswiseman76 <github@jameswiseman.com>
2015-09-17 15:18:26 +0100

    New version of Guard

--- JsGoogleCompile/Guard.cs
+++ JsGoogleCompile/Guard.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Linq.Expressions;
+    using System.Reflection;
 
     /// <summary>
     /// Static class for helping with guards
@@ -18,10 +19,13 @@
         /// <exception cref="ArgumentNullException">Exception thrown in the event of parameter being null</exception>
         public static void NotNull<T>(Expression<Func<T>> variable) where T : class 
         {
-            var value = variable.Compile().Invoke();
+            var memberSelector = (MemberExpression)variable.Body;
+            var constantSelector = (ConstantExpression)memberSelector.Expression;
+            var value = ((FieldInfo)memberSelector.Member).GetValue(constantSelector.Value);
+ 
             if (value == null)
             {
-                string name = ((MemberExpression)variable.Body).Member.Name;
+                var name = ((MemberExpression)variable.Body).Member.Name;
                 throw new ArgumentNullException(name);
             }
         }
