03f325a32088fe539f451980accb4a4dab13dd3d
jameswiseman76 <github@jameswiseman.com>
2015-10-15 23:14:25 +0100

    More unit tests

--- JsGoogleCompile.Tests/JSGoogleCompile.CLI/ArgumentRules/IsValidWarningSuppressionArgumentTests.cs
+++ JsGoogleCompile.Tests/JSGoogleCompile.CLI/ArgumentRules/IsValidWarningSuppressionArgumentTests.cs
@@ -1,10 +1,12 @@
 ﻿namespace JsGoogleCompile.Tests
 {
     using System;
+    using System.Linq;
 
     using JsGoogleCompile.CLI;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
+    using System.Collections.Generic;
 
     [TestClass]
     public class IsValidWarningSuppressionArgumentTests
@@ -22,6 +24,26 @@
         {
             var rule = new IsValidWarningSuppressionArgument(Mock.Of<ICommandLineArguments>());
             rule.IsSatisfiedBy(null);
+        }
+
+        [TestMethod]
+        public void Single_Valid_Warning_Argument_Is_Recognised()
+        {
+            // Arrange
+            var expectedWarningsSuppressed = new[] { "Error" };
+            var expectedSwitch = string.Format("/s{0}", string.Join(";", expectedWarningsSuppressed));
+            var commandLineArguments = new Mock<ICommandLineArguments>();
+            var rule = new IsValidWarningSuppressionArgument(Mock.Of<ICommandLineArguments>());
+            commandLineArguments.SetupAllProperties();
+
+            // Act
+            var isValid = rule.IsSatisfiedBy(new[] { expectedSwitch });
+
+            // Assert
+            Assert.IsTrue(isValid);
+            // todo: sort these out:
+            // commandLineArguments.VerifySet(m => m.SuppressedWarnings = It.Is<List<string>>(l => l.SequenceEqual(expectedWarningsSuppressed)));
+            // commandLineArguments.VerifySet(m => m.SuppressedWarnings = It.IsAny<List<string>>());
         }
     }
 }
