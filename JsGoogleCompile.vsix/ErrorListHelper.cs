9a8876edda421407c515d216783e063e091159d3
jameswiseman76 <github@jameswiseman.com>
2015-11-01 20:26:09 +0000

    Added VSIX project code.

---
+++ JsGoogleCompile.vsix/ErrorListHelper.cs
@@ -0,0 +1,133 @@
+﻿namespace JsGoogleCompile.vsix
+{
+    using System;
+
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.TextManager.Interop;
+    using System.Diagnostics;
+
+    public class ErrorListHelper : IServiceProvider
+    {
+        public static ErrorListProvider errorListProvider { get; set; }
+
+        public ErrorListHelper()
+        {
+            if (errorListProvider == null)
+            {
+                errorListProvider = GetErrorListProvider();
+            }
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return Package.GetGlobalService(serviceType);
+        }
+
+        public ErrorListProvider GetErrorListProvider()
+        {
+            ErrorListProvider provider = new ErrorListProvider(this);//this implementing IServiceProvider
+            provider.ProviderName = "JS Google Closure Compiler";
+            provider.ProviderGuid = Guid.Parse("DD9ACCF5-3445-452F-92A1-0649774B6E32");
+            return provider;
+        }
+
+        public void Write(
+                    TaskCategory category,
+                    TaskErrorCategory errorCategory,
+                    string context, //used as an indicator when removing
+                    string text,
+                    string document,
+                    int line,
+                    int column)
+        {
+            ErrorTask task = new ErrorTask();
+            task.Text = text;
+            task.ErrorCategory = errorCategory;
+            //The task list does +1 before showing this numbers
+            task.Line = line - 1;
+            task.Column = column - 1;
+            task.Document = document;
+            task.Category = category;
+
+            if (!string.IsNullOrEmpty(document))
+            {
+                //attach to the navigate event
+                task.Navigate += NavigateDocument;
+            }
+
+            //add it to the errorlistprovider
+            errorListProvider.Tasks.Add(task);
+        }
+
+        public void Remove()
+        {
+            errorListProvider.Tasks.Clear();
+        }
+
+        public static void OpenDocumentAndNavigateTo(
+                                            string path, int line, int column)
+        {
+            IVsUIShellOpenDocument openDoc = Package.GetGlobalService(typeof(IVsUIShellOpenDocument)) as IVsUIShellOpenDocument;
+
+            if (openDoc == null)
+            {
+                return;
+            }
+
+            IVsWindowFrame frame;
+            Microsoft.VisualStudio.OLE.Interop.IServiceProvider sp;
+            IVsUIHierarchy hier;
+            uint itemid;
+            Guid logicalView = VSConstants.LOGVIEWID_Code;
+
+            if (ErrorHandler.Failed(
+                openDoc.OpenDocumentViaProject(path, ref logicalView, out sp, out hier, out itemid, out frame))
+                || frame == null)
+            {
+                return;
+            }
+
+            object docData;
+            frame.GetProperty((int)__VSFPROPID.VSFPROPID_DocData, out docData);
+
+            // Get the VsTextBuffer  
+            VsTextBuffer buffer = docData as VsTextBuffer;
+            if (buffer == null)
+            {
+                IVsTextBufferProvider bufferProvider = docData as IVsTextBufferProvider;
+                if (bufferProvider != null)
+                {
+                    IVsTextLines lines;
+                    ErrorHandler.ThrowOnFailure(bufferProvider.GetTextBuffer(out lines));
+                    buffer = lines as VsTextBuffer;
+                    Debug.Assert(buffer != null, "IVsTextLines does not implement IVsTextBuffer");
+                    if (buffer == null)
+                    {
+                        return;
+                    }
+                }
+            }
+            // Finally, perform the navigation.  
+            IVsTextManager mgr = Package.GetGlobalService(typeof(VsTextManagerClass))
+                 as IVsTextManager;
+            if (mgr == null)
+            {
+                return;
+            }
+            mgr.NavigateToLineAndColumn(buffer, ref logicalView, line, column, line, column);
+        }
+
+        void NavigateDocument(object sender, EventArgs e)
+        {
+            Task task = sender as Task;
+            if (task == null)
+            {
+                throw new ArgumentException("sender");
+            }
+            //use the helper class to handle the navigation
+            OpenDocumentAndNavigateTo(task.Document, task.Line, task.Column);
+        }
+    }
+}
---
+++ JsGoogleCompile.vsix/JsGoogleCompile.cs
@@ -0,0 +1,157 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="JsGoogleCompile.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace JsGoogleCompile.vsix
+{
+    using System;
+    using System.ComponentModel.Design;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class JsGoogleCompile
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("8b2c61b8-73ec-406f-82b5-8dae60b0b39b");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        private static ErrorListHelper errorListHelper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsGoogleCompile"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private JsGoogleCompile(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            if (errorListHelper == null)
+            {
+                errorListHelper = new ErrorListHelper();
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static JsGoogleCompile Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new JsGoogleCompile(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            const string CompilerUrl = @"http://closure-compiler.appspot.com/compile";
+
+            var requestCompile = new RequestCompile(
+                "sample.js",
+                CompilationLevelHelper.AdvancedOptimizations,
+                CompilerUrl);
+
+            errorListHelper.Remove();
+
+            var compilerResults = requestCompile.Run();
+            var errorCount = compilerResults.Errors == null ? 0 : compilerResults.Errors.Count;
+            if (errorCount > 0)
+            {
+                foreach (var compilerError in compilerResults.Errors)
+                {
+                    errorListHelper.Write(
+                        TaskCategory.Misc,
+                        TaskErrorCategory.Error,
+                        "Some Context",
+                        compilerError.Error,
+                        "sample.js",
+                        compilerError.Lineno,
+                        compilerError.Charno);
+                }
+            }
+
+            var warningCount = compilerResults.Warnings == null ? 0 : compilerResults.Warnings.Count;
+            if (warningCount > 0)
+            {
+                foreach (var compilerWarning in compilerResults.Warnings)
+                {
+                    errorListHelper.Write(
+                        TaskCategory.Misc,
+                        TaskErrorCategory.Warning,
+                        "Some Context",
+                        compilerWarning.Warning,
+                        "sample.js",
+                        compilerWarning.Lineno,
+                        compilerWarning.Charno);
+                }
+            }
+
+            // Show a message box to prove we were here
+            ////string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            ////string title = "JsGoogleCompile";
+            ////VsShellUtilities.ShowMessageBox(
+            ////    this.ServiceProvider,
+            ////    message,
+            ////    title,
+            ////    OLEMSGICON.OLEMSGICON_INFO,
+            ////    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+            ////    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+            ////helper.Remove();
+        }
+    }
+}
