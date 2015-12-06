852d656a667284c224c1132a6841cb496745fd05
jameswiseman76 <github@jameswiseman.com>
2015-12-06 21:16:50 +0000

    More unit tests

--- JsGoogleCompile.Tests/JSGoogleCompile/ConsoleEmitterTests.cs
+++ JsGoogleCompile.Tests/JSGoogleCompile/ConsoleEmitterTests.cs
@@ -20,11 +20,13 @@
         }
 
         [TestMethod]
-        public void EmitWarnings_Logs_Message()
+        public void EmitWarnings_Logs_Message_Twice_With_Single_Warning()
         {
+            // Arrange
             var loggerMock = new Mock<ILog>();
             ConsoleEmitter.SetLogger(loggerMock.Object);
 
+            var expectedLineText = "expectedLineText" + Guid.NewGuid();
             var compilerResults = new CompilerResults
             {
                 OutputFilePath = string.Empty,
@@ -35,15 +37,60 @@
                        Lineno = 0,
                        Type = string.Empty,
                        Error = string.Empty,
-                       Line = string.Empty
+                       Line = expectedLineText
                     }
                 },
             };
             var emitter = new ConsoleEmitter();
 
+            // Act
             emitter.EmitWarnings(compilerResults);
 
+            // Assert
             loggerMock.Verify(m => m.Info(It.IsAny<string>()), Times.Exactly(2));
+            loggerMock.Verify(m => m.Info(It.Is<string>(p => p == expectedLineText)), Times.Once);
+        }
+
+        [TestMethod]
+        public void EmitWarnings_Logs_Message_Once_With_Valid_Results_That_Has_Empty_Warnings_Collection()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILog>();
+            ConsoleEmitter.SetLogger(loggerMock.Object);
+
+            var compilerResults = new CompilerResults
+            {
+                OutputFilePath = string.Empty,
+                Warnings = new List<CompilerError>()    // our empty warnings collection
+            };
+            var emitter = new ConsoleEmitter();
+
+            // Act
+            emitter.EmitWarnings(compilerResults);
+
+            // Assert
+            loggerMock.Verify(m => m.Info(It.IsAny<string>()), Times.Never);
+        }
+
+        [TestMethod]
+        public void EmitWarnings_Logs_Message_Once_With_Valid_Results_That_Has_Null_Warnings_Collection()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILog>();
+            ConsoleEmitter.SetLogger(loggerMock.Object);
+
+            var compilerResults = new CompilerResults
+            {
+                OutputFilePath = string.Empty,
+                Warnings = null     // our null warnings collection
+            };
+            var emitter = new ConsoleEmitter();
+
+            // Act
+            emitter.EmitWarnings(compilerResults);
+
+            // Assert
+            loggerMock.Verify(m => m.Info(It.IsAny<string>()), Times.Never);
         }
 
         [TestMethod]
